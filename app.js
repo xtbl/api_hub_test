dfd815bf212084d11a0952700a4e8d7b6fc7d98f
xtbl <correodecristobal@gmail.com>
2014-02-22 23:32:06 -0600

    add: basic GET data from API middleware added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var employee = require('./routes/employee');
+var getJSON = require('./routes/getJSON');
 var http = require('http');
 var path = require('path');
 
@@ -29,9 +30,35 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.use(function(request, response, next){
+    console.log('middleware: req is ' + request.method + 'and res is ' + response.url);
+    next();
+});
+
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/employees', employee.list);
+app.get('/getjson', getJSON.get);
+
+var options = {
+    host: 'api.bitcoinaverage.com',
+    port: 443,
+    path: '/ticker/global/all',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+var getJSONResultCallBack = function(statusCode, result){
+    // I could work with the result html/json here.  I could also just return it
+    console.log("onResult: (" + statusCode + ")" + JSON.stringify(result));
+    //        res.statusCode = statusCode;
+    //        res.send(result);
+    };
+
+getJSON.get(options, getJSONResultCallBack);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
